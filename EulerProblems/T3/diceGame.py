aab986e4f74024bfd078b4ccd809b01fd6b2209b
ZimneJonas <ZimneJonas@users.noreply.github.com>
2020-08-21 19:25:10 +0200

    Doing Stuff

---
+++ EulerProblems/T3/diceGame.py
@@ -0,0 +1,23 @@
+# Euler Difficulty 15%
+
+def diceGame():
+    count = 0
+    rounds = 0
+    pyrValue = 0
+    
+    for ii in range(1,5):
+    
+    for oo in range(0,9) #nine Dices
+        
+            pyrValue
+
+
+            for pp in range(0,6): #six Dices
+                for zz in range(1,7):
+                    rounds += 1
+                    if (ii*oo > zz*pp):
+                        count += 1
+    print(count/rounds)
+
+
+diceGame()
